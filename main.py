eccbd41b83d02fb1bc54f3d90c08fdbc8fd3de1d
Yemisi Oloruntoba <59591803+yemisi25@users.noreply.github.com>
2021-05-05 16:40:30 +0100

    Add files via upload
    Questions.
    
    Given some sample data, write a program to answer the following: (link to data set here https://docs.google.com/spreadsheets/d/16i38oonuX1y1g7C_UAmiK9GkY7cS-64DfiDMNiR41LM/edit#gid=0)
    
    On Shopify, we have exactly 100 sneaker shops, and each of these shops sells only one model of shoe. We want to do some analysis of the average order value (AOV). When we look at orders data over a 30 day window, we naively calculate an AOV of $3145.13. Given that we know these shops are selling sneakers, a relatively affordable item, something seems wrong with our analysis. 
    
    Think about what could be going wrong with our calculation. Think about a better way to evaluate this data.
    What metric would you report for this dataset?
    What is its value?
    Answers.
    1. The incorrect AOV calculation of $3145.13 was as a result of using incorrect : count()  function to calculate the total items istead of : sum() .
    Count() only provides the total count of the number of rows, while sum() will provide the AOV value by adding together all the values in the item column.
    
    2. To determine the correct Average Order Value (AOV), the reporting metrics are the respective sums of both 'order_amount' and 'total_items':
    
    oa_sum = data_df['order_amount'].sum()
    ti_sum = data_df['total_items'].sum()
    
    Next divide the total order amount (oa_sum) by the total items amount (ti_sum):
    AOV = oa_sum/ti_sum
    
    The Average Order Value (AOV) is: $357.92

---
+++ main.py
@@ -0,0 +1,77 @@
+# Import library
+import os
+import csv
+from pathlib import Path
+
+# Import Dependencies
+import pandas as pd
+import numpy as np
+
+# Set path to collect the data
+data = os.path.join("C:\\Users\\USER\\Downloads", '2019_Winter_Data_Science_Intern_Challenge_Data_Set.csv')
+
+
+csv_path = data
+
+# Setting the name and path for file to output
+file_to_output = "data_results.txt"
+
+# Import the data set as a DataFrame
+data_df = pd.read_csv(csv_path, encoding="utf-8")
+data_df.head()
+
+# Identify columns
+#data_df.columns
+
+# Identify data info
+#data_df.info()
+
+# Question 2: What metric would you report for this dataset?
+
+# Reporting metrics:
+
+# Order Amount (Sum total over the reporting period of the month of March)
+# Total Items (Sum total over the reporting period of the month of  March)
+
+# To determine Average Order Value (AOV)
+
+# First determine respective sums of both 'order_amount' and 'total_items'
+oa_sum = data_df['order_amount'].sum()
+ti_sum = data_df['total_items'].sum()
+print(oa_sum)
+print(ti_sum)
+
+# Then calculate average by dividing the total order amount (oa_sum) by the total items amount (ti_sum)
+AOV = oa_sum/ti_sum
+AOV
+
+# Print Average Order Value (AOV) with only 2 decimal places and the dollar sign
+print("%.2f" % AOV)
+
+# Print correct explanation with $
+'${:,.2f}'.format(AOV)
+
+# Incorrect calculation explained as a mistaken count() instead of sum() function
+oa_sum = data_df['order_amount'].sum()
+ti_count = data_df['total_items'].count()
+AOV = oa_sum/ti_count
+print(AOV)
+
+
+
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
