7e8ea78a2f438f07982473d80ef1be57093459e1
Yemisi Oloruntoba <59591803+yemisi25@users.noreply.github.com>
2021-05-06 16:39:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -61,8 +61,6 @@ print(AOV)
 
 # This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
 def print_hi(name):
